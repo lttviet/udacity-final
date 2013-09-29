0cd019c413f549a1770ccb3725ef82d431b2cc79
lttviet <lttviet@gmail.com>
2013-09-30 02:35:13 +0700

    Update wikipage.py

--- wikipage.py
+++ wikipage.py
@@ -9,8 +9,19 @@ from basehandler import BaseHandler
 class WikiPage(BaseHandler):
     def get(self, PAGE_RE):
         p = utils.get_page(PAGE_RE)
+        username = self.get_username()
+        login = True if username else False
+
         if p is None:
-            self.redirect('/_edit' + PAGE_RE)
+            if login:
+                content = ''    # empty content
+                p = utils.create_page(PAGE_RE, content)
+                self.redirect('/_edit' + PAGE_RE)
+            else:
+                self.response.write('This page does not exist. '
+                                    'Please log in to create it.')
         else:
             self.render('/templates/wikipage.html',
-                        content=p.content)
+                        login=login,
+                        username=username,
+                        page=p)
