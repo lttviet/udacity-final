7d26f7c16b7b33ae0c011bb2db588b056fe90e3e
lttviet <lttviet@gmail.com>
2013-09-30 02:31:14 +0700

    Fix logic bugs and add historypage handler

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from login import Login
 from logout import Logout
 from wikipage import WikiPage
 from editpage import EditPage
+from historypage import HistoryPage
 
 
 PAGE_RE = r'(/(?:[a-zA-Z0-9_-]+/?)*)'
@@ -17,6 +18,7 @@ app = webapp2.WSGIApplication([
     ('/signup', Signup),
     ('/login', Login),
     ('/logout', Logout),
+    ('/_edit' + PAGE_RE, EditPage),
+    ('/_history' + PAGE_RE, HistoryPage),
     (PAGE_RE, WikiPage),
-    ('/_edit' + PAGE_RE, EditPage)
 ], debug=True)
